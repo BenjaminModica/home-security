93ed9b86442ce05f05128f2909cf0ee33e0dfe77
Benjamin Modica <modica.benjamin@gmail.com>
2021-11-24 18:21:44 +0100

    Added pir sensor

--- ESP8266_Server_V2/ESP8266_Server_V2.ino
+++ ESP8266_Server_V2/ESP8266_Server_V2.ino
@@ -17,8 +17,13 @@
 ESP8266WebServer server(80);
 
 const int readyLed = 13;
-const int onLed = 15;
-const int button = 2;
+const int onLed1 = 0;
+const int onLed2 = 12;
+//const int button = 15; Has pulldown resistor, great for using as button!
+//const int trigPin = 5;
+//const int echoPin = 4;
+const int pirSensor = 16;
+
 
 //STATES
 //0: Alarm OFF
@@ -65,13 +70,18 @@ void handleStatusRequest() {
 void setup(void) {
   //pin and serial setup
   pinMode(readyLed, OUTPUT);
-  pinMode(onLed, OUTPUT);
-  pinMode(button, INPUT);
+  pinMode(onLed1, OUTPUT);
+  pinMode(onLed2, OUTPUT);
+  //pinMode(button, INPUT);
+  //pinMode(trigPin, OUTPUT); 
+  //pinMode(echoPin, INPUT); //Don't forget to input 3.3V and not 5V from motion sensor!
   digitalWrite(readyLed, 0);
-  digitalWrite(onLed, 0);
+  digitalWrite(onLed1, 0);
+  digitalWrite(onLed2, 0);
   Serial.begin(115200);
   //pinMode(LED_BUILTIN, OUTPUT);
   //digitalWrite(LED_BUILTIN, 1);
+  pinMode(pirSensor, INPUT);
 
   //wifi setup
   WiFi.mode(WIFI_STA);
@@ -112,7 +122,13 @@ void loop(void) {
   server.handleClient();
   MDNS.update();
   updateState();
-  if (digitalRead(button) == 0 && state == 1) {state = 2; Serial.println("Alarm tripped");} //Only trip alarm if set to ready
+  //distanceSensor();
+  if (state == 2) {loopLEDs();}
+
+  if (digitalRead(pirSensor) == HIGH && state == 1) {
+    state = 2;
+    Serial.println("detects stuff");
+  }
 }
 
 /**
@@ -122,16 +138,60 @@ void updateState() {
   switch (state) { 
     case 0: //Alarm off
       digitalWrite(readyLed, 0);
-      digitalWrite(onLed, 0);
+      digitalWrite(onLed1, 0);
+      digitalWrite(onLed2, 0);
       break;
     case 1: //Alarm ready
       digitalWrite(readyLed, 1);
-      digitalWrite(onLed, 0);
+      digitalWrite(onLed1, 0);
+      digitalWrite(onLed2, 0);
       break;
     case 2: //Alarm on
-      digitalWrite(onLed, 1);
       break;
     default:
       break;
   }
+}
+
+/**
+ * Takes care of everything to do with the distance sensor
+ */
+//void distanceSensor() {
+//  digitalWrite(trigPin, LOW);
+//  delayMicroseconds(2);
+//  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(10);
+//  digitalWrite(trigPin, LOW);
+//  
+//  const unsigned long duration= pulseIn(echoPin, HIGH);
+//  int distance= duration/29/2;
+//
+//  if(duration==0){
+//   Serial.println("Warning: no pulse from sensor");
+//  } 
+//  else{
+//      //For debugging purposes:
+//      //Serial.print("distance to nearest object:");
+//      //Serial.println(distance);
+//      //Serial.println(" cm");
+//  }
+//  delay(100);
+//
+//    if (distance < 10 && state == 1) { //Only trip alarm if set to ready
+//    state = 2; Serial.println("Alarm tripped");
+//  } 
+//}
+
+/*
+ * Flashes two LEDs if alarm is tripped
+ */
+void loopLEDs() {
+  if (!digitalRead(onLed1)) {
+    digitalWrite(onLed1, HIGH);
+    digitalWrite(onLed2, LOW);
+  } else {
+    digitalWrite(onLed1, LOW);
+    digitalWrite(onLed2, HIGH);
+  }
+  delay(100);
 }
